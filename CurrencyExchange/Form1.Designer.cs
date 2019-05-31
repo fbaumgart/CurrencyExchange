d2d6fb16ea34885a61b2b8ee02f3629ce04c22e4
fbaumgart <f.m.baumgart@hotmail.com>
2019-05-31 23:08:10 +0200

    first commit

---
+++ CurrencyExchange/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace CurrencyExchange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fromList = new System.Windows.Forms.ListBox();
+            this.toList = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.convertFrom_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exchangeRateLabel = new System.Windows.Forms.Label();
+            this.displayLabel = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fromList
+            // 
+            this.fromList.FormattingEnabled = true;
+            this.fromList.Items.AddRange(new object[] {
+            "PLN",
+            "EUR",
+            "GBP",
+            "USD"});
+            this.fromList.Location = new System.Drawing.Point(6, 28);
+            this.fromList.Name = "fromList";
+            this.fromList.Size = new System.Drawing.Size(100, 56);
+            this.fromList.TabIndex = 0;
+            this.fromList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // toList
+            // 
+            this.toList.FormattingEnabled = true;
+            this.toList.Items.AddRange(new object[] {
+            "PLN",
+            "EUR",
+            "GBP",
+            "USD"});
+            this.toList.Location = new System.Drawing.Point(6, 118);
+            this.toList.Name = "toList";
+            this.toList.Size = new System.Drawing.Size(100, 56);
+            this.toList.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.exchangeRateLabel);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.toList);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.convertFrom_box);
+            this.groupBox1.Controls.Add(this.fromList);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(407, 206);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // convertFrom_box
+            // 
+            this.convertFrom_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.convertFrom_box.Location = new System.Drawing.Point(143, 36);
+            this.convertFrom_box.Name = "convertFrom_box";
+            this.convertFrom_box.Size = new System.Drawing.Size(100, 26);
+            this.convertFrom_box.TabIndex = 5;
+            this.convertFrom_box.TextChanged += new System.EventHandler(this.convertFrom_box_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Konwerter walut - Autor: Filip Baumgart - CDV 2017 ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(143, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Przelicz walutę";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // exchangeRateLabel
+            // 
+            this.exchangeRateLabel.AutoSize = true;
+            this.exchangeRateLabel.Location = new System.Drawing.Point(284, 84);
+            this.exchangeRateLabel.Name = "exchangeRateLabel";
+            this.exchangeRateLabel.Size = new System.Drawing.Size(0, 13);
+            this.exchangeRateLabel.TabIndex = 5;
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.AutoSize = true;
+            this.displayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.displayLabel.Location = new System.Drawing.Point(26, 12);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(0, 13);
+            this.displayLabel.TabIndex = 8;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.displayLabel);
+            this.groupBox2.Location = new System.Drawing.Point(276, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(100, 35);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(140, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Wprowadź kwotę:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Przelicz na:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Przelicz z:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Po przeliczeniu:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(431, 230);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox fromList;
+        private System.Windows.Forms.ListBox toList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox convertFrom_box;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label exchangeRateLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label displayLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CurrencyExchange/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyExchange
+{
+    public partial class Form1 : Form
+    {
+        double amount;
+        double w_pln;
+        double exchangeValue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fromList.SelectedItem.ToString() == toList.SelectedItem.ToString())
+            {
+                displayLabel.Text = convertFrom_box.Text;
+            }
+            #region PLNtoOTHER
+            if ((fromList.SelectedItem.ToString() == "PLN") && (toList.SelectedItem.ToString() == "USD"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount * 0.28;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 USD = 3.57 PLN";
+            }
+            if ((fromList.SelectedItem.ToString() == "PLN") && (toList.SelectedItem.ToString() == "EUR"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount * 0.24;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 EUR = 4.20 PLN";
+            }
+            if ((fromList.SelectedItem.ToString() == "PLN") && (toList.SelectedItem.ToString() == "GBP"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount * 0.21;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 GBP = 4.79 PLN";
+            }
+            #endregion
+            #region EURtoOTHER
+            if ((fromList.SelectedItem.ToString() == "EUR") && (toList.SelectedItem.ToString() == "PLN"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount / 0.24;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 EUR = 4.20 PLN";
+            }
+            if ((fromList.SelectedItem.ToString() == "EUR") && (toList.SelectedItem.ToString() == "USD"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount / 0.24 * 0.28;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 EUR = 1.18 USD";
+            }
+            if ((fromList.SelectedItem.ToString() == "EUR") && (toList.SelectedItem.ToString() == "GBP"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount / 0.24 * 0.21;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 EUR = 0.88 GBP";
+            }
+            #endregion
+            #region USDtoOTHER
+            if ((fromList.SelectedItem.ToString() == "USD") && (toList.SelectedItem.ToString() == "PLN"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount / 0.28;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 USD = 3.57 PLN";
+            }
+            if ((fromList.SelectedItem.ToString() == "USD") && (toList.SelectedItem.ToString() == "EUR"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount / 0.28 * 0.24;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 USD = 0.85 USD";
+            }
+            if ((fromList.SelectedItem.ToString() == "USD") && (toList.SelectedItem.ToString() == "GBP"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount / 0.28 * 0.21;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 USD = 0.75 GBP";
+            }
+            #endregion
+            #region GBPtoOTHER
+            if ((fromList.SelectedItem.ToString() == "GBP") && (toList.SelectedItem.ToString() == "PLN"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount / 0.21;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 PLN = 4.79 GBP";
+            }
+            if ((fromList.SelectedItem.ToString() == "GBP") && (toList.SelectedItem.ToString() == "USD"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount / 0.24 * 0.28;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 EUR = 1.18 USD";
+            }
+            if ((fromList.SelectedItem.ToString() == "GBP") && (toList.SelectedItem.ToString() == "EUR"))
+            {
+                amount = Convert.ToDouble(convertFrom_box.Text);
+                exchangeValue = amount / 0.24 * 0.21;
+                displayLabel.Text = exchangeValue.ToString() + toList.SelectedItem.ToString();
+                exchangeRateLabel.Text = "1 EUR = 0.88 GBP";
+            }
+            #endregion
+        }
+
+        private void convertFrom_box_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
